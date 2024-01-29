6beb851edd01828ffa01479f674f14a1071aa5af
rockgard3n <liamcashel@gmail.com>
2024-01-29 14:35:00 -0800

    Update ContentView.swift

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -54,6 +54,7 @@ struct ContentView: View {
                 
                 Section("Amount per person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle(tipPercentage == 0 ? .red : .black)
                 }
                 
                 Section("Summary") {
